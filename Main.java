1338fa5cbec2b2025a53f41a91cb454ce103e76c
TejaAmbati-02 <73156927+TejaAmbati-02@users.noreply.github.com>
2021-09-29 14:43:58 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
-import java.lang;
-class Main
-{
-  public static void main(String args[]){
-  System.out.println("Hello World");
-  System.out.println("Teja");    
-}
+class Main{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        }
 }
